8a1c7d8c4345947e25e5bd3c56b6838d44799bf2
Marianna Kaledina <kamariann@yandex.ru>
2021-12-16 18:01:34 +0300

    modalmesto

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,48 @@ let profileName = document.querySelector('.profile__title');
 let nameInput = document.querySelector('.popup__input_type_name'); // Находим поля формы в DOM
 let profession = document.querySelector('.profile__subtitle');
 let jobInput = document.querySelector('.popup__input_type_work');
+const formMesto = document.querySelector('.popup__field_mesto');
+const imageName = document.querySelector('.popup__input_image_name');
+const imageSrc = document.querySelector('.popup__input_image_src');
+const elementTemplate = document.querySelector('.element').content;
+const elementList = document.querySelector('.elements');
+const elementText = document.querySelector('.element__text');
+const elementLike = document.querySelector('.element__like');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function (element) {
+  const initialCardsElement = elementTemplate.cloneNode(true);
+  initialCardsElement.querySelector('.element__image').src = element.link;
+  initialCardsElement.querySelector('.element__title').textContent = element.name;
+  elementList.append(initialCardsElement);
+
+})
 
 function openPopup() {
   element.classList.add('popup_opened');
